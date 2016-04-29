e5dc28104883a96f59344c27f1a378dd26eeedf7
Kirill Kuznetsov <kirill.kuznets@gmail.com>
2016-04-29 18:08:22 +0300

    Setup + WordNate base

---
+++ alg/wordnet/src/WordNet.java
@@ -0,0 +1,75 @@
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Out;
+
+import java.util.*;
+
+/**
+ * Created by kkuznets on 29.04.2016.
+ */
+public class WordNet {
+    private Digraph net;
+    private Set<String> nouns = new HashSet<>();
+    private String[] synsets;
+    // constructor takes the name of the two input files
+    public WordNet(String synsetsPath, String hypernymsPath){
+        if(synsetsPath == null || hypernymsPath == null){
+            throw new NullPointerException();
+        }
+        In synsetsIn = new In(synsetsPath);
+        List<String> sList = new ArrayList<>();
+        while(synsetsIn.hasNextLine()){
+            String synset = synsetsIn.readLine().split(",")[1];
+            sList.add(synset);
+            Collections.addAll(nouns, synset.split(" "));
+        }
+        synsetsIn.close();
+        synsets =  sList.toArray(new String[sList.size()]);
+        net = new Digraph(synsets.length);
+        In hypernymsIn = new In(hypernymsPath);
+        while(hypernymsIn.hasNextLine()){
+            String[] h = hypernymsIn.readLine().split(",");
+            if(h.length > 1) {
+                int hypernym = Integer.parseInt(h[0]);
+                for (int i = 1; i < h.length; i++){
+                    net.addEdge(Integer.parseInt(h[i]), hypernym);
+                }
+            }
+        }
+        hypernymsIn.close();
+    }
+
+    // returns all WordNet nouns
+    public Iterable<String> nouns(){
+        return nouns;
+    }
+
+    // is the word a WordNet noun?
+    public boolean isNoun(String word){
+        return nouns.contains(word);
+    }
+
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB){
+        return 0;
+    }
+
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path (defined below)
+    public String sap(String nounA, String nounB){
+        return "";
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args){
+        String path = "wordnet\\";
+        String synsetsPath = path + "synsets3.txt";
+        String hypernymsPath = path + "hypernyms3empty.txt";
+        WordNet wn = new WordNet(synsetsPath, hypernymsPath);
+        System.out.println(wn.nouns());
+        System.out.println("a " + wn.isNoun("a"));
+        System.out.println("c " + wn.isNoun("c"));
+        System.out.println("asd " + wn.isNoun("asd"));
+    }
+
+}
